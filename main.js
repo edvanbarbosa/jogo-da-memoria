7821c18d2fa6882fb7a9e9a8e58ebe0f45be9682
edvanbarbosa <edvabarbosa279@gmail.com>
2023-08-14 16:19:20 -0300

    atualizações para restart

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const divs = document.querySelectorAll('.game-envelope > div');
+const divs = document.querySelectorAll('.game-envelope  div');
 const ids = ['html','git','css',
             'js','react','python',
             'nodejs','c-sharp','ts',
@@ -22,20 +22,72 @@ const colors = {
     java: '#ec8950',
     sass: '#c69',
 }
-const NumberidsSorteados = [];
+let NumberidsSorteados = [];
 const min = document.querySelector('.minutos');
 const seg = document.querySelector('.segundos');
 const acertos = document.querySelector('#acertos')
 const erros = document.querySelector('#erros')
-const play = document.querySelector(".play")
-let statusCronos = false;
+const play = document.querySelectorAll("button")
+const divResult = document.querySelector('.result')
+const fecharDiv = document.querySelector('#fechar')
+let statusCronos = restart = false;
 let segundos = 0;
 let minutos = 0;
+let segDjogo = 0
+let minDjogo = 0
 let acertosCount = 0;
 let errosCount = 0;
 let NumeroSorteado;
 
+
+const viewDivs = ()=>{
+    divs.forEach(e=>{
+        if(e.style.visibility != 'visible'){
+            e.style.animation = "viewDiv .5s ease-in-out forwards"
+            e.style.visibility = 'visible'
+        }
+        else{
+            return
+        }
+        
+       
+    })
+}
+
+const result = ()=>{
+    divResult.style.display = 'flex' 
+
+    if(acertosCount >= 12){
+        divResult.querySelector('.result-envelope').innerHTML = `
+            <div class="result-content">
+            <h1 style = "color: #d9d38b;">Parabéns!</h1>
+            <p>Você conseguiu terminar o jogo em <span id="minutos">${minDjogo}</span> minutos e <span id="segundos" >${segDjogo}</span> segundos.</p>
+            <p>Você fez <span id="acertos" style="color:#8bd9b1;">${acertosCount}</span> acertos e <span id="erros" style = "color: #ee8787 ;">${errosCount}</span> erros.</p>
+            <button class="restart" onclick="Restart()">Jogar novamente</button>
+            </div>`
+    }else if(minutos<=0 && segundos <=0 && acertosCount< 12){
+        divResult.querySelector('.result-envelope').innerHTML = `
+            <div class="result-content">
+            <h1 style= "color: #ee8787;">O tempo Acabou :( </h1>
+
+            <p>Você fez <span id="acertos" style="color:#8bd9b1;">${acertosCount}</span> acertos e <span id="erros" style = "color: #ee8787;">${errosCount}</span> erros.</p>
+            <button class="restart" onclick="Restart()">Jogar novamente</button>
+            </div>`
+    }
+    else if(errosCount >=25){
+        divResult.querySelector('.result-envelope').innerHTML = `
+        
+        <div class="result-content">
+        <h1 style= "color:#ee8787;">25 erros é demais da conta </h1>
+        <p>Você fez <span id="acertos" style="color:#8bd9b1;">${acertosCount}</span> acertos e <span id="erros" style = "color: #ee8787 ;">${errosCount}</span> erros.</p>
+        <button class="restart" onclick="Restart()">Jogar novamente</button>
+        </div>`
+    }
+    
+}
+
 const sortearIds = ()=>{
+    NumberidsSorteados = []
     for(i in ids){
         NumeroSorteado = Math.floor(Math.random()*(ids.length))
         while(NumberidsSorteados.includes(NumeroSorteado)){
@@ -66,24 +118,67 @@ const AnimationsDesvirar = (e)=>{
     e.querySelector('img').style.animation = 'imgDesaparece 1s ease-in-out forwards'
 }
 
+
+const controlTempo = ()=>{
+        setInterval(()=>{
+            if(statusCronos == true){
+                if(minutos<=0 && segundos <=0){
+                    statusCronos = false
+                    result()
+                
+               }
+                if(acertosCount >= 12){
+                    statusCronos = false
+                    result()
+               }
+               if(errosCount >=25){
+                    statusCronos = false
+                    result()
+                
+               }
+               if(statusCronos == true){
+                if (minutos >=0){
+                    if (segundos == 0){
+                        segundos = 60
+                        segDjogo = 0
+                        minutos --
+                        minDjogo ++
+                    }
+                    segundos --
+                    segDjogo ++
+                    if (segundos < 10){
+                        seg.innerHTML = `0${segundos}`
+                    }else{
+                        seg.innerHTML = segundos
+                    }
+                    if (minutos < 10){
+                        min.innerHTML = `0${minutos}`
+                    }else{
+                        min.innerHTML = minutos
+                    }
+                }else{
+                    segundos = 0
+                    minutos = 2
+                    
+                }
+            }else{
+                return
+                
+            }
+        }
+            
+        },1000)
+    
+}
+
 const controlClicks = ()=>{
     let controlClick = [false, false]
     let controlDivs = []
-    
-        play.addEventListener('click',()=>{
-            if(statusCronos == false){
-            minutos = 2
-            statusCronos = true
-            }
-        })
-    
-    
     divs.forEach((e)=>{
        
             e.addEventListener('click',()=>{
                
                 if(statusCronos){ 
-        
                         if(controlClick[0] == false){
                             AnimationsVirar(e)
                             controlDivs[0] = e
@@ -99,6 +194,7 @@ const controlClicks = ()=>{
                                     controlDivs.forEach((e)=>{
                                         e.style.visibility = 'hidden';
                                         AnimationsDesvirar(e)
+                                        e.id = ''
                                     })
                                     acertosCount++
                                     acertos.innerHTML = acertosCount
@@ -119,67 +215,46 @@ const controlClicks = ()=>{
             })
     })
 }
-const controlTempo = ()=>{
+const playGame = ()=>{
+    statusCronos= restart = true
+    acertosCount  =  errosCount =  0
     
-        setInterval(()=>{
-
-           
-
-
-          
-           
-            if(statusCronos == true){
-                if(minutos<=0 && segundos <=0){
-                    alert('O tempo acabooooouuu!')
-                    statusCronos = false
-                    acertosCount  =  errosCount =  0
-                    minutos = 2
-                    segundos = 1
-               }
-                if(acertosCount >= 12){
-                    alert('Você ganhou!')
-                    statusCronos = false
-                    acertosCount  =  errosCount =  0
-                    minutos = 2
-                    segundos = 1
-               }
-               if(errosCount >=25){
-                    alert('25 erros é demais da conta!')
-                    statusCronos = false
-                    acertosCount  =  errosCount =  0
-                    minutos = 2
-                    segundos = 1
-               }
-                if (minutos >=0){
-                    if (segundos == 0){
-                        segundos = 60
-                        minutos --
-                    }
-                    segundos --
-                    if (segundos < 10){
-                        seg.innerHTML = `0${segundos}`
-                    }else{
-                        seg.innerHTML = segundos
-                    }
-                    if (minutos < 10){
-                        min.innerHTML = `0${minutos}`
-                    }else{
-                        min.innerHTML = minutos
-                    }
-                }
-            }else{
-                segundos = segundos
-                minutos = minutos
-                
-            }
-            
-            
-        },1000)
+    minutos = 2
+    segundos = 0
+    viewDivs()
+    sortearIds();
+    atribuirImagens()
+    controlTempo()
+    controlClicks()
+   
+   
     
+    
+        
+   
 }
 
-controlClicks()
-controlTempo()
-sortearIds();
+const Restart = ()=>{
+    acertosCount  =  errosCount =  0
+    erros.innerHTML = errosCount
+    acertos.innerHTML = acertosCount
+    minutos = 2
+    segundos = 0
+    viewDivs()
+    sortearIds();
+    atribuirImagens()
+    statusCronos = true
+    divResult.style.display = 'none'
 
-atribuirImagens()
+}
+
+play.forEach(e=>{
+    e.addEventListener('click',()=>{
+        if(!statusCronos){
+           restart == false? playGame():Restart()
+        }
+        
+    })
+})
+
+
